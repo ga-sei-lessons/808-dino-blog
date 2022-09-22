25a1f8a1d10933983aa1b7c403ca1f5f6389bbaf
weston-bailey <wdbailey87@gmail.com>
2022-09-22 12:29:21 -0700

    makes dino blog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Post from './Post'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const post = {
+  title: "Dinosaurs are awesome",
+  author: "Stealthy Stegosaurus",
+  body: "Check out this body property!",
+  comments: ["First!", "Great post", "Hire this author now!"]
 }
 
-export default App;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Post 
+          title={post.title}
+          author={post.author}
+          body={post.body}
+          comments={post.comments}
+        />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/Post.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import Comment from './Comment'
+
+class Post extends Component {
+    render() {
+        console.log(this)
+        const comments = this.props.comments.map((comment, idx) => {
+            return (
+                <Comment 
+                    key={`comment${idx}`}
+                    commentText={comment}
+                />
+            )
+        })
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+                
+                <p>by: {this.props.author}</p>
+
+                <p>{this.props.body}</p>
+
+                <h2>Comments:</h2>
+
+                {comments}
+            </div>
+        )
+    }
+}
+
+export default Post
